667f52e2e7288f45954f0ffdb1f5f73940c554c0
ArminIrannejad <armin.irannejad@gmail.com>
2024-03-28 12:58:33 +0100

    Neovim config added

---
+++ .config/nvim/lua/armino112/init.lua
@@ -0,0 +1,2 @@
+require ("armino112.remap")
+require ("armino112.set")
---
+++ .config/nvim/lua/armino112/set.lua
@@ -0,0 +1,38 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.api.nvim_exec([[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * lua vim.highlight.on_yank {higroup="IncSearch", timeout=200}
+  augroup END
+]], false)
+
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
