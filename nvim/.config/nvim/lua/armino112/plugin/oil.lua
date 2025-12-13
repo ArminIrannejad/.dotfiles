c8acdc14fdd3683b5c0dfbcd0386a73c23d75c3a
ArminIrannejad <armin.irannejad@gmail.com>
2025-12-13 18:29:17 +0100

    chore: includs permissions when viewing in oil

--- nvim/.config/nvim/lua/armino112/plugin/oil.lua
+++ nvim/.config/nvim/lua/armino112/plugin/oil.lua
@@ -4,28 +4,43 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       CustomOilBar = function()
-        local path = vim.fn.expand "%"
+        local path = vim.fn.expand("%")
         path = path:gsub("oil://", "")
-
         return "  " .. vim.fn.fnamemodify(path, ":.")
       end
 
-      require("oil").setup {
-        columns = { "icon" },
+      require("oil").setup({
+        columns = {
+          "icon",
+          "permissions",
+          "size",
+          "mtime",
+        },
+
         keymaps = {
           ["<C-h>"] = false,
-          ["<C-l>"] = false,
-          ["<C-k>"] = false,
-          ["<C-j>"] = false,
-          ["<M-h>"] = "actions.select_split",
+          ["<BS>"] = "actions.parent",
+          ["C"] = "actions.cd",
+          ["~"] = {
+            callback = function()
+              require("oil").open(vim.fn.expand("~"))
+            end,
+          },
         },
+
         win_options = {
-          winbar = "%{v:lua.CustomOilBar()}",
+          winbar = nil
         },
+
         view_options = {
           show_hidden = true,
+          is_always_hidden = function(name)
+            return name == ".." or name == "../"
+          end,
+
         },
-      }
+      })
+
       vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end,
   },
