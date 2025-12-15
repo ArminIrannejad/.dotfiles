e15c232936406e93f06c30f2f14a7b5f8d04846e
ArminIrannejad <armin.irannejad@gmail.com>
2025-12-15 21:02:56 +0100

    chore: adds orgmode bullets

--- nvim/.config/nvim/lua/armino112/plugin/org.lua
+++ nvim/.config/nvim/lua/armino112/plugin/org.lua
@@ -1,21 +1,42 @@
 return {
   {
-    'nvim-orgmode/orgmode',
-    event = 'VeryLazy',
-    ft = { 'org' },
+    "nvim-orgmode/orgmode",
+    ft = { "org" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
     config = function()
       -- Setup orgmode
-      require('orgmode').setup({
-        org_agenda_files = '~/orgfiles/**/*',
-        org_default_notes_file = '~/orgfiles/refile.org',
+      require("orgmode").setup({
+        org_agenda_files = { "~/orgfiles/**/*" }, -- you can keep this
+        org_default_notes_file = "~/orgfiles/refile.org",
       })
-
       -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
       -- add ~org~ to ignore_install
       -- require('nvim-treesitter.configs').setup({
       --   ensure_installed = 'all',
       --   ignore_install = { 'org' },
       -- })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "org",
+        callback = function()
+          vim.wo.conceallevel = 2
+          vim.wo.concealcursor = "nc"
+        end,
+      })
     end,
-  }
+  },
+
+  {
+    "nvim-orgmode/org-bullets.nvim",
+    ft = { "org" },
+    dependencies = { "nvim-orgmode/orgmode" },
+    config = function()
+      require("org-bullets").setup({
+        symbols = { "◉", "○", "●", "○", "●", "○", "●" },
+        -- symbols = { "●", "○", "◆", "◇", "▸", "▹" },
+      })
+    end,
+  },
 }
