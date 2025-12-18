4cafd451e1b7c441147f9fcfdce6b3ec51e3934a
ArminIrannejad <armin.irannejad@gmail.com>
2025-12-18 14:48:09 +0100

    chore: ignore install for orgmode

--- nvim/.config/nvim/lua/armino112/plugin/treesitter.lua
+++ nvim/.config/nvim/lua/armino112/plugin/treesitter.lua
@@ -1,17 +1,22 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup {
-                ensure_installed = { "python", "c", "vim", "lua", "query", "markdown", "bash", "markdown_inline"},
-                ignore_install = {"org"},
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                },
-                additional_vim_regex_highlighting = false,
-            }
-        end,
-    }
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = { "python", "c", "vim", "lua", "query", "markdown", "bash", "markdown_inline" },
+        ignore_install = { "org", "grammar", },
+        auto_install = true,
+        highlight = {
+          enable = true,
+          disable = { "org", "grammar", },
+        },
+        indent = {
+          enable = true,
+          disable = { "org", "grammar", },
+        },
+        additional_vim_regex_highlighting = false,
+      }
+    end,
+  }
 }
