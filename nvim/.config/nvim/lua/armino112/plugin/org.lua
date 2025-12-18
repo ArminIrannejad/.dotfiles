a4af348984ed4108ce56597f9b68196055e1f6e8
ArminIrannejad <armin.irannejad@gmail.com>
2025-12-18 14:49:01 +0100

    chore: don't use conceal for bullets

--- nvim/.config/nvim/lua/armino112/plugin/org.lua
+++ nvim/.config/nvim/lua/armino112/plugin/org.lua
@@ -7,25 +7,18 @@ return {
     },
     config = function()
       -- Setup orgmode
+      vim.g.orgmode_treesitter_installed = false
       require("orgmode").setup({
-        org_agenda_files = { "~/orgfiles/**/*" }, -- you can keep this
+        org_agenda_files = { "~/orgfiles/**/*" },
         org_default_notes_file = "~/orgfiles/refile.org",
       })
-      -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
-      -- add ~org~ to ignore_install
-      -- require('nvim-treesitter.configs').setup({
-      --   ensure_installed = 'all',
-      --   ignore_install = { 'org' },
-      -- })
-
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = "org",
-        callback = function()
-          vim.wo.conceallevel = 2
-          vim.wo.concealcursor = "nc"
-        end,
-      })
-    end,
+    end
+    -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+    -- add ~org~ to ignore_install
+    -- require('nvim-treesitter.configs').setup({
+    --   ensure_installed = 'all',
+    --   ignore_install = { 'org' },
+    -- })
   },
 
   {
@@ -34,7 +27,7 @@ return {
     dependencies = { "nvim-orgmode/orgmode" },
     config = function()
       require("org-bullets").setup({
-        symbols = { "◉", "○", "●", "○", "●", "○", "●" },
+        -- symbols = { "◉", "○", "●", "○", "●", "○", "●" },
         -- symbols = { "●", "○", "◆", "◇", "▸", "▹" },
       })
     end,
