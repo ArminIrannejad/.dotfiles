12c6cf72e8db53120d7d3059ba6610a1f921ffdd
ArminIrannejad <armin.irannejad@gmail.com>
2026-01-26 14:43:45 +0100

    chore(git): changes diffviewer

--- nvim/.config/nvim/lua/armino112/plugin/git.lua
+++ nvim/.config/nvim/lua/armino112/plugin/git.lua
@@ -4,25 +4,12 @@ return {
     lazy = true,
     dependencies = {
       "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
       "nvim-telescope/telescope.nvim",
     },
     cmd = "Neogit",
     keys = {
-      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
-    },
-    config = true,
-  },
-
-  {
-    "esmuellert/vscode-diff.nvim",
-    lazy = true,
-    dependencies = { "MunifTanjim/nui.nvim" },
-    cmd = "CodeDiff",
-    keys = {
-      { "<leader>do", "<cmd>CodeDiff<cr>" },
-      { "<leader>df", "<cmd>CodeDiff file HEAD<cr>" },
-      { "<leader>dc", "<cmd>tabclose<cr>"},
-    },
-  },
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+    }
+  }
 }
-
