c882f5ccb4af8b19b899f94b6f3f08679f71151a
ArminIrannejad <armin.irannejad@gmail.com>
2026-02-26 17:24:16 +0100

    feat: new plugin

---
+++ nvim/.config/nvim/lua/armino112/plugin/abolish.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "tpope/vim-abolish",
+    cmd = { "Abolish", "Subvert", "S" },
+    keys = {
+      { "crs", mode = "n" },
+      { "crm", mode = "n" },
+      { "crc", mode = "n" },
+      { "cru", mode = "n" },
+      { "crU", mode = "n" },
+      { "cr-", mode = "n" },
+      { "cr.", mode = "n" },
+      { "cr_", mode = "n" },
+      { "cr ", mode = "n" },
+      { "crk", mode = "n" },
+      { "crt", mode = "n" },
+    },
+    init = function()
+    end,
+    config = function()
+    end,
+  },
+}
