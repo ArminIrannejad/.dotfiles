db06b59aad0b51c3ec20b31058292ee0cbcb09a9
ArminIrannejad <armin.irannejad@gmail.com>
2026-03-25 11:20:53 +0100

    chore: scrolloff

--- nvim/.config/nvim/lua/armino112/set.lua
+++ nvim/.config/nvim/lua/armino112/set.lua
@@ -46,7 +46,7 @@ vim.opt.incsearch = true
 vim.opt.cursorline = false
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 12
+vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
