0154ee6c19419dfb908936e771c496ec6ba20664
ArminIrannejad <armin.irannejad@gmail.com>
2026-03-25 11:21:54 +0100

    chore: removes unneccessary binds

--- nvim/.config/nvim/lua/armino112/plugin/abolish.lua
+++ nvim/.config/nvim/lua/armino112/plugin/abolish.lua
@@ -7,13 +7,7 @@ return {
       { "crm", mode = "n" },
       { "crc", mode = "n" },
       { "cru", mode = "n" },
-      { "crU", mode = "n" },
-      { "cr-", mode = "n" },
-      { "cr.", mode = "n" },
-      { "cr_", mode = "n" },
-      { "cr ", mode = "n" },
       { "crk", mode = "n" },
-      { "crt", mode = "n" },
     },
     init = function()
     end,
