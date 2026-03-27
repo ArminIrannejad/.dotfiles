805bc5dc1b6eec51e32ff0b03444f41810733661
ArminIrannejad <armin.irannejad@gmail.com>
2026-03-27 10:08:55 +0100

    chore: ugly hack to get static cwd

--- nvim/.config/nvim/lua/armino112/telescope/multigrep.lua
+++ nvim/.config/nvim/lua/armino112/telescope/multigrep.lua
@@ -46,8 +46,52 @@ local live_multigrep = function(opts)
   }):find()
 end
 
+local live_multigrep_current = function(opts)
+  opts = opts or {}
+
+  opts.cwd = "~/work"
+
+  local finder = finders.new_async_job {
+    command_generator = function(prompt)
+      if not prompt or prompt == "" then
+        return nil
+      end
+
+      local pieces = vim.split(prompt, "  ")
+      local args = { "rg" }
+
+      if pieces[1] then
+        table.insert(args, "-e")
+        table.insert(args, pieces[1])
+      end
+
+      if pieces[2] then
+        table.insert(args, "-g")
+        table.insert(args, pieces[2])
+      end
+
+      ---@diagnostic disable-next-line: deprecated
+      return vim.tbl_flatten {
+        args,
+        { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
+      }
+    end,
+    entry_maker = make_entry.gen_from_vimgrep(opts),
+    cwd = opts.cwd,
+  }
+
+  pickers.new(opts, {
+    debounce = 100,
+    prompt_title = "Multi Grep Current",
+    finder = finder,
+    previewer = conf.grep_previewer(opts),
+    sorter = require("telescope.sorters").empty(),
+  }):find()
+end
+
 M.setup = function()
   vim.keymap.set("n", "<leader>fg", live_multigrep)
+  vim.keymap.set("n", "<leader>gc", live_multigrep_current)
 end
 
 return M
