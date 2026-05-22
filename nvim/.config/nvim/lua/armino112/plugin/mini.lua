5ebdd359bde2c2fcada273f27e08684250accc54
ArminIrannejad <armin.irannejad@gmail.com>
2026-05-22 11:26:41 +0200

    Trying mini.jump

--- nvim/.config/nvim/lua/armino112/plugin/mini.lua
+++ nvim/.config/nvim/lua/armino112/plugin/mini.lua
@@ -1,9 +1,11 @@
 vim.pack.add({
   { src = 'https://github.com/echasnovski/mini.nvim' },
+  { src = 'https://github.com/nvim-mini/mini.jump', version = 'stable' },
   -- { src = 'https://github.com/nvim-mini/mini.pairs'},
 })
 
 require("mini.surround").setup({})
+require("mini.jump").setup({})
 
 -- require("mini.pairs").setup({
 --   modes = { insert = true, command = true, terminal = false },
