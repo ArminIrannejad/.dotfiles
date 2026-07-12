dcade369f23d3ff875f5a97ea1e2572b144b99a3
ArminIrannejad <armin.irannejad@gmail.com>
2026-07-12 12:30:33 +0200

    Fix race cond

--- roles/neovim/files/lua/armino112/plugin/haskell.lua
+++ roles/neovim/files/lua/armino112/plugin/haskell.lua
@@ -2,3 +2,14 @@ vim.pack.add({
   { src = 'https://github.com/mrcjkb/haskell-tools.nvim' },
   { src = 'https://github.com/neovimhaskell/haskell-vim' },
 })
+
+vim.lsp.config("haskell-tools.nvim", {
+  on_init = function(client)
+    vim.api.nvim_create_autocmd("VimLeavePre", {
+      group = vim.api.nvim_create_augroup("haskell-tools-hls-clean-exit-" .. client.id, { clear = true }),
+      callback = function()
+        client:stop(false)
+      end,
+    })
+  end,
+})
