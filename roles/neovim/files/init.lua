821c2bfe26454519e31dbd87dbec91f5d4f43c32
ArminIrannejad <armin.irannejad@gmail.com>
2026-07-12 12:16:27 +0200

    Haskell change

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -1,6 +1,3 @@
 vim.loader.enable()
-local ui2_ok, ui2 = pcall(require, "vim._core.ui2")
-if ui2_ok then
-  ui2.enable({})
-end
+require('vim._core.ui2').enable({})
 require("armino112")
--- roles/neovim/files/lua/armino112/plugin/haskell.lua
+++ roles/neovim/files/lua/armino112/plugin/haskell.lua
@@ -1,4 +1,4 @@
 vim.pack.add({
-  { src = 'https://github.com/mrcjkb/haskell-tools.nvim', version = "v10.0.0" },
+  { src = 'https://github.com/mrcjkb/haskell-tools.nvim' },
   { src = 'https://github.com/neovimhaskell/haskell-vim' },
 })
