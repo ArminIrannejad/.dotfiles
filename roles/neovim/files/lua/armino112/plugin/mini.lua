4504449cfe0d43b7319e661a1c7cf2138843b6b6
ArminIrannejad <armin.irannejad@gmail.com>
2026-07-15 02:25:49 +0200

    Backward jump

--- roles/neovim/files/lua/armino112/plugin/mini.lua
+++ roles/neovim/files/lua/armino112/plugin/mini.lua
@@ -9,6 +9,12 @@ require("mini.surround").setup({})
 require("mini.jump").setup({})
 require("mini.ai").setup({})
 
+vim.keymap.set({ "n", "x", "o" }, ",", function()
+  local backward = MiniJump.state.backward
+  MiniJump.jump(nil, not backward)
+  MiniJump.state.backward = backward
+end, { desc = "Jump to previous target" })
+
 -- require("mini.pairs").setup({
 --   modes = { insert = true, command = true, terminal = false },
 --   skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
