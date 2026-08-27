1846b8a0901008bbaf5b62b426dd556424180bde
ArminIrannejad <armin.irannejad@gmail.com>
2026-08-27 12:52:11 +0200

    Full MINI

--- roles/neovim/files/lua/armino112/plugin/mini.lua
+++ roles/neovim/files/lua/armino112/plugin/mini.lua
@@ -1,5 +1,5 @@
 vim.pack.add({
-  -- { src = 'https://github.com/echasnovski/mini.nvim' },
+  { src = 'https://github.com/echasnovski/mini.nvim' },
   -- { src = 'https://github.com/nvim-mini/mini.pairs'},
   { src = 'https://github.com/nvim-mini/mini.jump',      version = 'stable' },
   { src = 'https://github.com/nvim-mini/mini.ai' },
